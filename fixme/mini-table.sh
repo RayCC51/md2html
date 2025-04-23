876244f6eb0fcc72a098fd6134f3aa638042cb5e
raycc51 <4q26dd@gmail.com>
2025-04-23 14:57:36 +0900

    give up table alignment

--- fixme/mini-table.sh
+++ fixme/mini-table.sh
@@ -5,47 +5,24 @@
 
 MOD=$(cat "$1")
 
-# table tr
-MOD=$(echo "$MOD" | sed -E '
-  /^\|(.*\|)+$/ {
-    s/^/<table>\n<tr>/
-    s/$/<\/tr>\n<\/table>/
+
+
+MOD=$(echo "$MOD" | sed '
+  /^|.*|$/ {
+    i\<table>
+    :a
+    $ a\</table>
+    n
+    /^|.*|$/ ba
+    i\</table>
   }
 ')
 
-# clean duplicated table
-MOD=$(echo "$MOD" | sed -E '
-  /^<\/table>$/ {
-    N
-    /^<\/table>\n<table>$/d
-  }
-')
 
-# thead tbody seperator
-MOD=$(echo "$MOD" | sed -E '
-  /^<table>$/,/^<\/table>$/ {
-    s/^<tr>[|:-]+<\/tr>$/<\/thead>\n<tbody>/
-  }
-')
 
-# td
-MOD=$(echo "$MOD" | sed -E '
-  /^<tr>/ {
-    s/^<tr>\|/<tr>\n    <td>/
-    s/\|<\/tr>$/<\/td>\n<\/tr>/
-    s/\|/<\/td>\n    <td>/g
-  }
-')
 
-# thead tbody
-MOD=$(echo "$MOD" | sed -E '
-  /^<table>$/ a\<thead>
-  /^<\/table>$/ i\<\/tbody>
-')
 
-# td -> th
-MOD=$(echo "$MOD" | sed -E '
-  /<thead>/,/<\/thead>/s/td>/th>/g
-')
-
+echo ""
+echo "++++++"
+echo ""
 echo "$MOD"
